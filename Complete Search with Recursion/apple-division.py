b33eb334885303d253c1abde9412cc92e38684f1
veer-p <veerp@student.ubc.ca>
2023-01-02 05:46:18 -0800

    add first module

---
+++ Complete Search with Recursion/apple-division.py
@@ -0,0 +1,10 @@
+n = int(input())
+weights = list(map(int, input().split()))
+
+def solve(i, sum1, sum2):
+	if i == n:
+		return abs(sum2 - sum1)
+	else:
+		return min(solve(i+1, sum1 + weights[i], sum2), solve(i+1, sum1, sum2 + weights[i]))
+
+print(solve(0,0,0))
---
+++ Complete Search with Recursion/creating-strings.py
@@ -0,0 +1,21 @@
+s = input()
+perms = []
+char_count = [0] * 26
+
+def search(curr):
+    if len(curr) == len(s):
+        perms.append(curr)
+    else:
+        for i in range(26):
+            if char_count[i] > 0:
+                char_count[i] -= 1
+                search(curr + chr(ord('a') + i))
+                char_count[i] += 1
+
+for c in s:
+    char_count[ord(c) - ord('a')] += 1
+
+search("")
+print(len(perms))
+for p in perms:
+    print(p)
